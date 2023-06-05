b3d36c743c36a156f5232ae58012b8dee7e5c88b
JuanHeza <juanehza@hotmail.com>
2023-06-05 19:29:04 +0000

    mini servidor

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello world!");
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Express app!')
+});
+
+app.listen(3000, () => {
+  console.log('server started');
+});
